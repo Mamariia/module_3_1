22224edbbf5e1fbf9de60ed5fa7e656e46c2a078
Mamariia <148610516+Mamariia@users.noreply.github.com>
2024-07-31 21:37:55 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,25 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info (string):
+    count_calls()
+    tuple_ = (len(string), string.upper(), string.lower())
+    return tuple_
+
+def is_contains (string, list_to_search):
+    count_calls()
+    string = string.lower()
+    list_to_search = [s.lower() for s in list_to_search]
+    if string in list_to_search:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
